98fd6a9d5a89f17e5d8a9785b1500131a6b1ed57
svsdval <svsdval@amd64x8>
2019-05-03 19:18:02 +0700

    Переделал на управление с ГУИ

--- README.md
+++ README.md
@@ -4,26 +4,36 @@ youtube synthesia video to midi, just for fun )
 Воссоздание midi с видео роликов synthesia и ей подобным..
 
 # dependency / зависимости
+
 - python-opencv
+- python-pygame
 - midiutil
 
-# install dependency / установка зависимостей 
+# install dependency / установка зависимостей.
   ```
-  sudo apt install python-opencv
+  sudo apt install python-opencv python-pygame
   pip install midiutil
   ```
 
 # usage / использование
+ RU:
+ Скачиваем видео с понравившейся милодией ( рекомендую 720p ) , запускаем программу, в ней регулируем клавиши жмём Q и после завершения работы будет создан midi файл.
+ 
+ EN:
+ Download the video with your favorite melody (I recommend 720p), launch the program, adjust the keys in it, press Q and after the completion of the work a midi file will be created.
+
   ```
   ./v2m.py ./test_video/Not\ Evil\ -\ The\ LEGO\ Movie\ 2\ -\ The\ Second\ Part\ _\ Piano\ Tutorial\ \(Synthesia\)-9mv_hKwCzUo.mkv
   ./v2m.py ./test_video/Gotham\ City\ Guys\ -\ The\ LEGO\ Movie\ 2\ -\ The\ Second\ Part\ _\ Piano\ Tutorial\ \(Synthesia\)-y_aBUQrp5vY.webm
   ```
 
-# in v2m.py parameters / параметры для изменения в исполняемом файле
-
-
-xoffset_whitekeys - начальное положение (на видео) клавиатуры по X
-yoffset_whitekeys - начальное положение (на видео) клавиатуры по Y
+  Управление:
+  * **Стрелки** - подстройка клавиш / keys adjustment
+  * **PageUp/PageDown** - прокрутка видео / scrolling video 
+  * **Q** - приступить к воссозданию midi / begin to recreate midi
+  * **ESCAPE** - выход / quit
+  * **Левая кнопка мыши** - перетаскивание выбранной клавиши / dragging the selected key
+  * **Правая кнопка мыши** - перетаскивание всех клавиш, если клавиша выбрана, перенос осуществляется относительно неё / dragging all keys, if the key is selected, the transfer is carried out relative to it.
 
 # how it works / как это работает
 
@@ -32,7 +42,3 @@ frame by frame we scan the virtual keyboard and write the keys to the midi file.
 Кадр за кадром сканируется видео поток отслеживая изменения виртуальной клавиатуры после всё что зафиксировано дампим на винт.
 
 ![Alt text](docs/frame47.jpg?raw=true "input from image")
-
-# todo / что сделать
-
-- gui to configure keyboard layouts / добавить гуи для управления виртуальной раскладкой клавиатуры ...
--- v2m.py
+++ v2m.py
@@ -1,60 +1,79 @@
 #!/usr/bin/python
 
+# by svsd_val
+# jabber : svsd_val@jabber.ru
+# mail to: svsdval@gmail.com
+
 import math;
-import cv2
-from PIL import Image
-from midiutil.MidiFile import MIDIFile
-import os
-import sys
-import ntpath
+import cv2;
+from PIL import Image;
+from midiutil.MidiFile import MIDIFile;
+import os;
+import sys;
+import ntpath;
+
+import pygame;
+from pygame.locals import *;
+
+from OpenGL.GL import *;
+from OpenGL.GLU import *;
+
+width=640;
+height=480;
+
+mpos = [0,0];
+
+xoffset_whitekeys = 60;
+yoffset_whitekeys = 673;
+
+yoffset_blackkeys = -30;
+
+keys_pos=[];
+keyp_colors = [ [241,173,64], [216,57,77], [218,52,64], [105,150,192], [39,87,149] ];
+
+keygrab=0;
+keygrabid=-1;
+
+whitekey_width=24.6;
 
 if ( len(sys.argv) < 2 ):
   print "halt, no args";
-  sys.exit( 0 ) 
+  sys.exit( 0 ) ;
 
 filepath = sys.argv[1];
 if os.path.exists( filepath ):
   print "open file " + filepath;
-  vidcap = cv2.VideoCapture( filepath )
+  vidcap = cv2.VideoCapture( filepath );
 else:
   print "file not exists " + filepath;
-  sys.exit( 0 ) 
+  sys.exit( 0 ) ;
 
 outputmid= ntpath.basename( filepath ) + "_output.mid";
 
-success,image = vidcap.read()
-frame = 308
+frame= 0;
+# set start frame;
+vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame);
+
+success,image = vidcap.read();
 
 debug = 0;
 resize= 1;
-debug_keys = 1;
+debug_keys = 0;
 
-length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-width  = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps    = float(vidcap.get(cv2.CAP_PROP_FPS))
-
-# set start frame
-vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame)
-
-success,image = vidcap.read()
-#
+length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT));
+width  = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH));
+height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT));
+fps    = float(vidcap.get(cv2.CAP_PROP_FPS));
+#;
 print "video fps: %d" % fps;
 
 
-# create  MIDI object
-mf = MIDIFile(1) # only 1 track
-track = 0 # the only track
 
-time = 0 # start at the beginning
-
-mf.addTrackName(track, time, "Sample Track")
-mf.addTempo(track, time, 60 )
-
-# add some notes
-channel = 0
-volume = 100
+# add some notes;
+channel = 0;
+volume = 100;
 basenote=35;
+
 
 notes=[];
 notes_db=[];
@@ -63,74 +82,153 @@ keys_pos=[];
 
 keyp_colors = [ [241,173,64], [216,57,77], [218,52,64], [105,150,192], [39,87,149], [166,250,103], [102,185,43] ];
 keyp1d = 90;
-#
+#;
 minimal_duration = 0.1;
+bgImgGL=-1;
 
-for i in range(96):
-  notes.append(0)
+for i in range(127):
+  notes.append(0);
   notes_db.append(0);
   notes_de.append(0);
-  #
-
-xoffset_whitekeys = 5 # 40 for avengers, else 5
-xoffset_whitekeys = 60
-
-yoffset_whitekeys = 603
-
-yoffset_blackkeys = yoffset_whitekeys - 30
-xoffset_blackkeys = xoffset_whitekeys + 10
-#
-width=1280;
-height=720;
-whitekey_width=24.6;
-debug= 0;
+#;
 resize= 0;
 
-xx=0
-for i in range(8):
- for j in range(12):
-  keys_pos.append( [0,0] )
-  keys_pos[i*12+j][0] = int(round(xoffset_whitekeys + xx ));
-  keys_pos[i*12+j][1] = yoffset_whitekeys;
-  if (j == 1) or ( j ==3 ) or ( j == 6 ) or ( j == 8) or ( j == 10 ):
-   xx += -whitekey_width;
-   keys_pos[i*12+j][0] = int(round(xoffset_whitekeys + xx  + whitekey_width *0.5 ));
-   keys_pos[i*12+j][1] = yoffset_blackkeys;
-  xx += whitekey_width
+if ( resize == 1 ):
+  width=1280;
+  height=720;
 
-while success:
+
+def updatekeys( append=0 ):
+ global keys_pos;
+ xx=0;
+ for i in range(8):
+  for j in range(12):
+   if (append == 1):
+    keys_pos.append( [0,0] );
+   keys_pos[i*12+j][0] = int(round( xx ));
+   keys_pos[i*12+j][1] = 0;
+   if (j == 1) or ( j ==3 ) or ( j == 6 ) or ( j == 8) or ( j == 10 ):
+    xx += -whitekey_width;
+    keys_pos[i*12+j][0] = int(round( xx  + whitekey_width *0.5 ));
+    keys_pos[i*12+j][1] = yoffset_blackkeys;
+   xx += whitekey_width;
+
+
+def loadImage(idframe=130):
+  vidcap.set(cv2.CAP_PROP_POS_FRAMES, idframe);
+  success,image = vidcap.read();
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, image );
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glEnable(GL_TEXTURE_2D);
+
+
+def DrawQuad(x,y,x2,y2, texx=1, texy=-1):
+  glBegin(GL_QUADS);
+  glTexCoord2f(0, texy);
+  glVertex2f(x, y);
+  glTexCoord2f(texx, texy);
+  glVertex2f(x2, y);
+  glTexCoord2f(texx, 0);
+  glVertex2f(x2, y2);
+  glTexCoord2f(0, 0);
+  glVertex2f(x, y2);
+  glEnd();
+
+updatekeys( 1 );
+
+def processmidi():
+ global frame;
+ global xoffset_whitekeys;
+ global yoffset_whitekeys;
+ global whitekey_width;
+ global bgImgGL;
+ global width;
+ global height;
+ global length;
+ global fps;
+
+ global notes;
+ global notes_db;
+ global notes_de;
+
+ # create  MIDI object;
+ mf = MIDIFile(1) # only 1 track;
+ track = 0 # the only track;
+
+ time = 0 # start at the beginning;
+
+ mf.addTrackName(track, time, "Sample Track");
+ mf.addTempo(track, time, 60 );
+
+ vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame);
+ success,image = vidcap.read();
+
+ while success:
+
   if (frame % 100 == 0):
-    print "processing frame: " + str(frame) + " / " + str(length);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+   glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, image );
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+   glEnable(GL_TEXTURE_2D);
+
+   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+   glViewport (0, 0, width, height);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   glOrtho(0, width, height, 0, -1, 100);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+
+   #glScale(scale,scale,1);
+   glColor4f(1.0, 1.0, 1.0, 1.0);
+
+   glEnable(GL_TEXTURE_2D);
+   DrawQuad(0,0,width,height);
+   glEnable(GL_BLEND);
+   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+   glColor4f(1.0, 0.5, 1.0, 0.5);
+   glDisable(GL_TEXTURE_2D);
+ #  glPushMatrix();
+ #  glTranslatef(0,height*0.5 - 20,0);
+   p= frame / float( length );
+   DrawQuad(0,height *0.5 -10, p  * width ,height *0.5 +10);
+ #  glPopMatrix();
+   pygame.display.flip();
+
+#  if (frame % 100 == 0):
+   print "processing frame: " + str(frame) + " / " + str(length) + " % " + str( math.trunc(p * 100));
+
   if ( resize == 1 ):
     image=cv2.resize(image, (width , height));
 
-  # processing white keys
-  for i in range( 96 ):
-    pixx=keys_pos[i][0]
-    pixy=keys_pos[i][1]
+  # processing white keys;
+  for i in range( len(keys_pos) ):
+    pixx=xoffset_whitekeys + keys_pos[i][0];
+    pixy=yoffset_whitekeys + keys_pos[i][1];
 
-    if ( pixx > width ) or ( pixy > height ): break;
-    if ( pixx < 0 ) or ( pixy < 0 ): continue;
+    if ( pixx > width ) or ( pixy > height ) or ( pixx < 0 ) or ( pixy < 0 ): continue;
 
-    keybgr=image[pixy,pixx]
+    keybgr=image[pixy,pixx];
     key= [ keybgr[2], keybgr[1],keybgr[0] ];
 
-    note=i;
+    note=(i+1);
 
     if ( debug == 1 ):
       cv2.rectangle(image, (pixx-5,pixy-5), (pixx+5,pixy+5), (255,0,255));
       cv2.rectangle(image, (pixx-1,pixy-1), (pixx+1,pixy+1), (255,0,255));
       cv2.putText(image, str(note), (pixx-5,pixy+20), 0, 0.5, (255,0,255));
- 
+
     keypressed=0;
 
     for keyc in keyp_colors:
       if ( abs( int(key[0]) - keyc[0] ) < keyp1d ) and ( abs( int(key[1]) - keyc[1] ) < keyp1d ) and ( abs( int(key[2]) - keyc[2] ) < keyp1d ):
        keypressed=1;
 
-    # reg pressed key
+    # reg pressed key;
     if keypressed==1:
-      # if key is not pressed
+      # if key is not pressed;
       if ( notes[note] == 0 ):
         if ( debug_keys == 1 ):
           print "white keys, note pressed on :" + str( note );
@@ -143,29 +241,211 @@ while success:
         notes[ note ] = 0;
         notes_de[ note ] = frame;
         time = notes_db[note] / fps;
-        duration = ( notes_de[note] - notes_db[note] ) / fps ;
+        duration = ( notes_de[note] - notes_db[note] ) / fps;
         if ( duration < minimal_duration ): duration = minimal_duration;
 
         if ( debug_keys == 1 ):
-          print "white keys, note released :" + str( note ) + " de = " + str(notes_de[note]) + "- db =" + str(notes_db[note]);
+          print "white keys, note released :" + str(note ) + " de = " + str(notes_de[note]) + "- db =" + str(notes_db[note]);
           print "midi add white keys, note : " +str(note) + " time:" +str(time) + " duration:" + str(duration);
-        mf.addNote(track, channel, basenote+ note, time , duration , volume )
+        mf.addNote(track, channel, basenote+ note, time , duration , volume );
 
   xapp=0;
-
-
-#  os.remove("/tmp/frame%d.jpg" % frame)
-
-  #cv2.imwrite("/tmp/frame%d.jpg" % frame, image)  # save frame as JPEG file
-
   if ( debug == 1 ):
     cv2.imwrite("/tmp/frame%d.jpg" % frame, image)  # save frame as JPEG file
 
-#  if frame > 0: 
-#    break;
-  success,image = vidcap.read()
-  frame += 1
+  success,image = vidcap.read();
+  frame += 1;
 
-# write midi to disk
-with open(outputmid, 'wb') as outf:
-    mf.writeFile(outf)
+# write midi to disk;
+ with open(outputmid, 'wb') as outf:
+  mf.writeFile(outf);
+
+
+def GenHelpTexture(text):
+  font = pygame.font.Font(None, 64)
+  textSurface = font.render(text, True, (255,255,255,255), (0,0,0,255))
+  ix, iy = textSurface.get_width(), textSurface.get_height()
+  img = pygame.image.tostring(textSurface, "RGBX", True)
+  glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+  i = glGenTextures(1)
+  glBindTexture(GL_TEXTURE_2D, i)
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, img)
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+  return i
+
+
+def main():
+  global xoffset_whitekeys;
+  global yoffset_whitekeys;
+  global yoffset_blackkeys;
+  global whitekey_width;
+  global bgImgGL;
+  global pyfont;
+  running=1;
+  keygrab=0;
+  keygrabid=-1;
+  keygrabaddx=0;
+
+  scale=1.0;
+  pygame.init();
+  pyfont = pygame.font.SysFont('Sans', 20)
+  display = (width,height);
+  screen = pygame.display.set_mode(display, DOUBLEBUF|OPENGL);
+
+  bgImgGL = glGenTextures(1);
+  glBindTexture(GL_TEXTURE_2D, bgImgGL);
+  loadImage();
+  #
+  helptext = GenHelpTexture("Q - for process, Mouse wheel & arrows to adjust keys, Right mouse button - to move all keys");
+
+  frame= 0;
+  # set start frame;
+  vidcap.set(cv2.CAP_PROP_POS_FRAMES, frame);
+
+  display = (width,height);
+  pygame.display.set_mode(display, DOUBLEBUF|OPENGL);
+  while running==1:
+    mousex, mousey = pygame.mouse.get_pos();
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glViewport (0, 0, width, height);
+    glMatrixMode (GL_PROJECTION);
+    glLoadIdentity ();
+    glOrtho(0, width, height, 0, -1, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glScale(scale,scale,1);
+    glColor4f(1.0, 1.0, 1.0, 1.0);
+
+    glBindTexture(GL_TEXTURE_2D, bgImgGL);
+    glEnable(GL_TEXTURE_2D);
+    DrawQuad(0,0,width,height);
+    glColor4f(1.0, 1.0, 1, 0.7);
+    glBindTexture(GL_TEXTURE_2D, helptext);
+    DrawQuad(0,0,width,height *0.05, texy=1);
+
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glColor4f(1.0, 0.5, 1.0, 0.5);
+    glPushMatrix();
+    glTranslatef(xoffset_whitekeys,yoffset_whitekeys,0);
+    glDisable(GL_TEXTURE_2D);
+    for i in range( len( keys_pos) ):
+     glPushMatrix();
+     glTranslatef(keys_pos[i][0],keys_pos[i][1],0);
+     DrawQuad(-5,-5,5,5);
+     DrawQuad(-1,-1,1,1);
+     glPopMatrix();
+     glColor4f(0.0, 1.0, 1.0, 0.5);
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(mousex,mousey,0);
+    glScalef(0.5,0.5,0.5);
+    DrawQuad(-5,-5,5,5);
+    glPopMatrix();
+
+    for event in pygame.event.get():
+     if event.type == pygame.QUIT: 
+      running = 0;
+      pygame.quit();
+      quit();
+     elif event.type == pygame.KEYDOWN:
+#      print event.key;
+      if event.key == pygame.K_q:
+       running = 0;
+      if event.key == pygame.K_ESCAPE:
+       running = 0;
+       pygame.quit();
+       quit();
+      if event.key == pygame.K_UP:
+       yoffset_blackkeys -= 1;
+       updatekeys( );
+      if event.key == pygame.K_DOWN:
+       yoffset_blackkeys += 1;
+       updatekeys( );
+      if event.key == pygame.K_LEFT:
+       whitekey_width-=0.1;
+       updatekeys( );
+      if event.key == pygame.K_RIGHT:
+       whitekey_width+=0.1;
+       updatekeys( );
+      if event.key == pygame.K_PAGEUP:
+       frame+=100;
+       if (frame > length *0.98):
+        frame=math.trunc(length *0.98);
+
+       glBindTexture(GL_TEXTURE_2D, bgImgGL);
+       loadImage(frame);
+      if event.key == pygame.K_PAGEDOWN:
+       frame-=100;
+       if (frame < 1):
+        frame=1;
+       glBindTexture(GL_TEXTURE_2D, bgImgGL);
+       loadImage(frame);
+
+     elif event.type == pygame.MOUSEBUTTONUP:
+      if ( event.button == 1 ):
+        keygrab = 0;
+        keygrabid = -1;
+      if ( event.button == 3 ):
+        keygrab = 0;
+
+     elif event.type == pygame.MOUSEBUTTONDOWN:
+#      print event.button;
+      if ( event.button == 4 ):
+        whitekey_width+=0.05;
+#        print "whitekey_width="+str(whitekey_width);
+        updatekeys( );
+#        scale+=0.1;
+      if ( event.button == 5 ):
+        whitekey_width-=0.05;
+#        print "whitekey_width="+str(whitekey_width);
+        updatekeys( );
+#        scale-=0.1;
+      if (scale >2): scale=2;
+      if (scale <1): scale=1;
+
+      if ( event.button == 1 ):
+        size=5;
+        print "x offset " + str(xoffset_whitekeys) + " y offset: " +str(yoffset_whitekeys);
+        for i in range( len( keys_pos) ):
+         if (abs( mousex - (keys_pos[i][0] + xoffset_whitekeys) )< size) and (abs( mousey - (keys_pos[i][1] + yoffset_whitekeys) )< size):
+          keygrab=1;
+          keygrabid=i;
+          print "ok click found on : "+str(keygrabid);
+          break;
+        pass;
+      if ( event.button == 3 ):
+        keygrab = 2;
+        size=5;
+        print "x offset " + str(xoffset_whitekeys) + " y offset: " +str(yoffset_whitekeys);
+        keygrabaddx=0
+        for i in range( len( keys_pos) ):
+         if (abs( mousex - (keys_pos[i][0] + xoffset_whitekeys) )< size) and (abs( mousey - (keys_pos[i][1] + yoffset_whitekeys) )< size):
+          keygrab=2;
+          keygrabaddx=keys_pos[i][0];
+          print "ok click found on : "+str(keygrabid);
+          break;
+
+    if ( keygrab == 1) and ( keygrabid >-1 ):
+#     print "moving keyid = " + str(keygrabid);
+     keys_pos[ keygrabid ][0] = mousex - xoffset_whitekeys;
+     keys_pos[ keygrabid ][1] = mousey - yoffset_whitekeys;
+    if ( keygrab == 2):
+#      print "moving offsets : "+ str(mousex) + " x " + str(mousey);
+      xoffset_whitekeys = mousex - keygrabaddx;
+      yoffset_whitekeys = mousey;
+
+    pygame.display.flip();
+
+main();
+
+frame=0;
+processmidi();
+
+print "done...";
