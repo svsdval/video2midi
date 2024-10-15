64605266b2fc4087a1b27b085679c15aff073ce4
Martin Rys <martin@rys.rs>
2024-10-15 23:44:39 +0200

    Fix overzealous tab replace

--- video2midi/prefs.py
+++ video2midi/prefs.py
@@ -26,7 +26,7 @@ class prefs:
 	rollcheck = False
 	rollcheck_priority = 0
 
-	keyp_colors_channel =	  [ 0,0, 1,1, 2,2, 3,3, 4,4, 5,5, 6,6, 7,7, 8,8, 9,9, 10,10, 11,11 ] # MIDI channel per color
+	keyp_colors_channel =      [ 0,0, 1,1, 2,2, 3,3, 4,4, 5,5, 6,6, 7,7, 8,8, 9,9, 10,10, 11,11 ] # MIDI channel per color
 	keyp_colors_channel_prog = [ 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0,  0, 0,  0, 0 ] # MIDI program ID per channel
 
 	xoffset_whitekeys = 60
